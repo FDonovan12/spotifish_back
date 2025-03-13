31106d5307979e085ccb77d14924a0416f6779cc
FDonovan12 <donovanferreira12@gmail.com>
2025-03-13 13:27:11 +0100

    start to modify CustomResponse

--- src/main/java/fr/donovan/spotifish/controller_api/AlbumControllerApi.java
+++ src/main/java/fr/donovan/spotifish/controller_api/AlbumControllerApi.java
@@ -1,6 +1,7 @@
 package fr.donovan.spotifish.controller_api;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import fr.donovan.spotifish.custom_response.CustomListResponse;
 import fr.donovan.spotifish.entity.Album;
 import fr.donovan.spotifish.custom_response.CustomResponse;
 import fr.donovan.spotifish.dto.AlbumDTO;
@@ -26,30 +27,30 @@ public class AlbumControllerApi {
     @GetMapping(path = UrlRoute.URL_ALBUM)
     @JsonView(JsonViews.AlbumListJsonViews.class)
     public CustomResponse<List<Album>> list() {
-        return new CustomResponse<>(HttpStatus.OK.value(), "AlbumControllerApi - list()", "Album", this.albumService.findAll());
+        return CustomListResponse.success(albumService.findAll());
     }
 
     @GetMapping(path = UrlRoute.URL_ALBUM + "/{id}")
     @JsonView(JsonViews.AlbumShowJsonViews.class)
     public CustomResponse<Album> show(@PathVariable String id) {
-        return new CustomResponse<>(HttpStatus.OK.value(), "AlbumControllerApi - show("+id+")", "Album", this.albumService.getObjectById(id));
+        return CustomResponse.success(albumService.getObjectById(id));
     }
     
     @PostMapping(path = UrlRoute.URL_ALBUM_NEW)
     @JsonView(JsonViews.AlbumShowJsonViews.class)
     @ResponseStatus(HttpStatus.CREATED)
     public CustomResponse<Album> create(@Valid @RequestBody AlbumDTO albumDTO) {
-        return new CustomResponse<>(HttpStatus.CREATED.value(), "AlbumControllerApi - create()", "Album", albumService.persist(albumDTO));
+        return CustomResponse.created(albumService.persist(albumDTO));
     }
     
-    @PutMapping(path = UrlRoute.URL_ALBUM_EDIT + "/{id}")
-    @JsonView(JsonViews.AlbumShowJsonViews.class)
-    public CustomResponse<Album> update(@Valid @RequestBody AlbumDTO albumDTO, @PathVariable String id) {
-        return new CustomResponse<>(HttpStatus.OK.value(), "AlbumControllerApi - update("+id+")", "Album", albumService.persist(albumDTO, id));
-    }
-    
-    @DeleteMapping(path = UrlRoute.URL_ALBUM_DELETE + "/{id}")
-    public CustomResponse<Boolean> delete(@PathVariable String id) {
-        return new CustomResponse<>(HttpStatus.OK.value(), "AlbumControllerApi - delete("+id+")", "Album", albumService.delete(id));
-    }
+//    @PutMapping(path = UrlRoute.URL_ALBUM_EDIT + "/{id}")
+//    @JsonView(JsonViews.AlbumShowJsonViews.class)
+//    public CustomResponse<Album> update(@Valid @RequestBody AlbumDTO albumDTO, @PathVariable String id) {
+//        return new CustomResponse<>(HttpStatus.OK.value(), "AlbumControllerApi - update("+id+")", "Album", albumService.persist(albumDTO, id));
+//    }
+//
+//    @DeleteMapping(path = UrlRoute.URL_ALBUM_DELETE + "/{id}")
+//    public CustomResponse<Boolean> delete(@PathVariable String id) {
+//        return new CustomResponse<>(HttpStatus.OK.value(), "AlbumControllerApi - delete("+id+")", "Album", albumService.delete(id));
+//    }
 }
